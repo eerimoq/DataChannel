63eadf5a791d7b5ac2ffd83885d23e75e876c9cc
Erik Moqvist <erik.moqvist@gmail.com>
2026-02-15 11:03:18 +0100

    Version 0.26.0.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .target(name: "Webrtc", dependencies: ["libdatachannel"]),
         .binaryTarget(name: "libdatachannel",
-                      url: "https://github.com/eerimoq/libdatachannel-xcframework/releases/download/v0.25.0/libdatachannel.xcframework.zip",
-                      checksum: "0052d6c90d898792523f1792c564a689fd1a6cc786b107a7f1e5b926ca8198a4"),
+                      url: "https://github.com/eerimoq/libdatachannel-xcframework/releases/download/v0.26.0/libdatachannel.xcframework.zip",
+                      checksum: "48c91ff56916cc9c01ccde35489ad87d8ff5124d5d395b5459f9e6a641152b86"),
     ]
 )
