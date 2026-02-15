5a358d97551f651fc1469640712b9c29cea8ffe9
Erik Moqvist <erik.moqvist@gmail.com>
2026-02-15 07:55:38 +0100

    Enums.

--- Package.swift
+++ Package.swift
@@ -5,13 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "Webrtc",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
         .library(
             name: "Webrtc",
-            targets: ["libdatachannel"]
+            targets: ["Webrtc", "libdatachannel"]
         ),
     ],
     targets: [
+        .target(name: "Webrtc", dependencies: ["libdatachannel"]),
         .binaryTarget(name: "libdatachannel",
                       url: "https://github.com/eerimoq/libdatachannel-xcframework/releases/download/v0.25.0/libdatachannel.xcframework.zip",
                       checksum: "0052d6c90d898792523f1792c564a689fd1a6cc786b107a7f1e5b926ca8198a4"),
--- Sources/Webrtc/Webrtc.swift
+++ Sources/Webrtc/Webrtc.swift
@@ -1,2 +1,48 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import libdatachannel
+
+public enum WebrtcConnectionState {
+    case new
+    case connecting
+    case connected
+    case disconnected
+    case failed
+    case closed
+
+    init?(value: rtcState) {
+        switch value {
+        case RTC_NEW:
+            self = .new
+        case RTC_CONNECTING:
+            self = .connecting
+        case RTC_CONNECTED:
+            self = .connected
+        case RTC_DISCONNECTED:
+            self = .disconnected
+        case RTC_FAILED:
+            self = .failed
+        case RTC_CLOSED:
+            self = .closed
+        default:
+            return nil
+        }
+    }
+}
+
+public enum WebrtcGatheringState {
+    case new
+    case inProgress
+    case complete
+
+    init?(value: rtcGatheringState) {
+        switch value {
+        case RTC_GATHERING_NEW:
+            self = .new
+        case RTC_GATHERING_INPROGRESS:
+            self = .inProgress
+        case RTC_GATHERING_COMPLETE:
+            self = .complete
+        default:
+            return nil
+        }
+    }
+}
