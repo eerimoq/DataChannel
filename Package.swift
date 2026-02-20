0084cbdd5adbe5f66967560f693b936f272516b7
Erik Moqvist <erik.moqvist@gmail.com>
2026-02-20 14:40:56 +0100

    New.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .target(name: "DataChannel", dependencies: ["libdatachannel"]),
         .binaryTarget(name: "libdatachannel",
-                      url: "https://github.com/eerimoq/xcframeworks/releases/download/libdatachannel-0.27.0/libdatachannel.xcframework.zip",
-                      checksum: "fc6e60d89558044028f24c4793310e4172c46fd984f80e9f9e76497cd5a46e10"),
+                      url: "https://github.com/eerimoq/xcframeworks/releases/download/libdatachannel-0.27.0-2/libdatachannel.xcframework.zip",
+                      checksum: "4e955ca212244b425d64309bb0064ece75e8c49d8cda45e446a677401764cf2a"),
     ]
 )
